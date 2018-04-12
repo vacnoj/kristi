47461f81f5656269c5c76ee039a8945419a13eec
Jon Caviness <Jon@jon-5.domain>
2018-04-11 18:35:19 -0600

    fixed reload error

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,6 @@
 $('document').ready(function() {
    console.log("Page Loaded");
 
-   $('.modal').modal({
-      complete: function() {
-      location.reload();
-      }
-   });
-
    $('.tabs').tabs();
 
    $('.materialboxed').materialbox();
@@ -17,6 +11,34 @@ $('document').ready(function() {
       console.log(e.target);
    });
 
+   var videos = {
+      video1: "https://www.youtube.com/embed/30oL2tZGxnY",
+      video2: "https://www.youtube.com/embed/xbLPAtLdalU",
+      video3: "https://www.youtube.com/embed/1FZD8atwErA",
+      video4: "https://www.youtube.com/embed/OSzqtK3rT8k",
+      video5: "https://www.youtube.com/embed/zypwkzCybWg",
+      video6: "https://www.youtube.com/embed/aOabSbw5p2M",
+      video7: "https://www.youtube.com/embed/1-OJSNfLFWs",
+      video8: "https://www.youtube.com/embed/HO27WKp-LTc",
+      video9: "https://www.youtube.com/embed/buMhjDIErzo",
+      video10: "https://www.youtube.com/embed/D7dfo_KkQvM"
+   }
 
+   $('.modal').modal({
+      complete: function() {
+      // location.reload();
+    
+         for (var i=1; i < 11; i++) {
+            let video = $("#video" + i).attr("src");
+            $("#video" + i).attr("src","");
+            $("#video" + i).attr("src",video);
+         }
+      }
 
 });
+
+});
+
+// var video = $("#playerid").attr("src");
+// $("#playerid").attr("src","");
+// $("#playerid").attr("src",video);
